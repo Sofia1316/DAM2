dea0b5014a0ed031f19879dfc323ceef82353fce
Sofia <sofiiaa1316@gmail.com>
2025-09-22 14:30:19 +0200

    actualizar main

--- programacionMultimedia/ejemploInicial00/main/java/com/example/hola/MainActivity.kt
+++ programacionMultimedia/ejemploInicial00/main/java/com/example/hola/MainActivity.kt
@@ -5,22 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.BlendMode
 //import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.LineHeightStyle
@@ -40,7 +50,7 @@ class MainActivity : ComponentActivity() {
             //DosTextosVertical()
             //dispos()
             //ejemploBox()
-            ImagenConTexto()
+            ImagenConTexto2()
         }
     }
 }
@@ -126,4 +136,52 @@ fun ImagenConTexto(){
 @Composable
 fun drogonView(){
     ImagenConTexto()
+}
+
+// EJEMPLO 5
+fun randomColor(): Color{ // para decir el parámetro de salida
+    /*palabra reservada para valores que van a cambiar --> val*/
+    /*palabra reservada para valores que no van a cambiar --> var*/
+    val rojo = (0..255).random()
+    val azul = (0..255).random()
+    val verde = (0..255).random()
+
+    return Color(rojo, azul, verde)
+}
+
+@Composable
+fun ImagenConTexto2(){
+    var BackgroundBoxColor by remember{ mutableStateOf(Color.White) }
+    var textPosition by remember{mutableStateOf(Offset(0f,0f))}
+    Box(modifier = Modifier.fillMaxSize().padding(25.dp).background(BackgroundBoxColor)){
+        Image(
+            painter = painterResource(id = R.drawable.drogon),
+            contentDescription = "Dragón GOT",
+            modifier = Modifier.align(Alignment.Center).fillMaxSize()
+        )
+        Text(text="Drogon",
+            fontSize = 22.sp,
+            color=Color.Green,
+            textAlign = TextAlign.Center, // centrado en la línea en la que estoy escribiendo
+            modifier = Modifier.align(Alignment.Center) //
+        )
+        Button(
+            onClick = {BackgroundBoxColor = randomColor()},
+            //modifier= Modifier.align(Alignment.TopStart)
+            // La mayus es una clase y la minuscula de offset para situar algún elemento
+            modifier= Modifier.offset{
+                intOffset(textPosicion.x.toInt(),textPosition.y.toInt())
+            }.pointerInput(Unit){
+                detectDragGestures { change, dragAmount ->  textPosition = Offset(dragAmount.x,dragAmount.y)}
+            }
+        )
+        {
+            Text(text="Cambiar fondo") // detrás de cada button, poner un text entre llaves
+        }
+    }
+}
+@Preview
+@Composable
+fun drogonView2(){
+    ImagenConTexto2()
 }
